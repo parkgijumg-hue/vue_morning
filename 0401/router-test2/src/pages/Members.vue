4f968271992723ad499ce05f5a7208c96ce3c955
박기정 <parkgijumg@gmail.com>
2026-04-01 15:02:41 +0900

    Update Members.vue for contribution test

--- 0401/router-test2/src/pages/Members.vue
+++ 0401/router-test2/src/pages/Members.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="card card-body">
     <h2 class="m-4">이날치 그룹Members</h2>
+    <!-- Updated for contribution test -->
     <div class="container">
       <div class="row">
         <!-- 한 가로줄에 컬럼을 지정할 수 있음. 최대크기에는 6개,중간에는 3~4개 , 최소2개이상 -->
